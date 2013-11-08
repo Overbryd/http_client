0ac9fba082f59d60d2ec2e01bd1bd2b728c67c23
Lukas Rieder <l.rieder@gmail.com>
2013-11-08 10:49:20 +0100

    trying to add tomdoc

--- lib/http_client.rb
+++ lib/http_client.rb
@@ -76,6 +76,21 @@ class HttpClient
 
   attr_reader :client, :max_retries, :response_class, :default_request_options
 
+  # Initialize a HttpClient
+  #
+  # options - The Hash options used to fine tune the client (optional):
+  #             :use_connection_pool        - Set this to true if you intend to use a capped connection pool (default: false).
+  #             :max_connections            - The maximum number of connections a pool will open (optional, default: 20, only when using a connection pool).
+  #             :max_connections_per_route  - The maximum number of connections that can be made to one server (optional, only when using a connection pool).
+  #             :max_retries                - The maximum number of retries the client will attempt on retryable exceptions (default: 0).
+  #             :response_class             - Use a different class to handle responses (default: HttpClient::Response).
+  #             :connection_request_timeout - Max wait time in milliseconds for a request when the connection pool has no free connection (default: 100, only when using a connection pool).
+  #             :connect_timeout            - Max wait time in milliseconds for establishing a connection to a server (default: 100).
+  #             :socket_timeout             - Max wait time in milliseconds for receiving a response from the server (default: 2000).
+  #             :socket_timeout             - Max wait time in milliseconds for receiving a response from the server (default: 2000).
+  #             :default_request_options    - Default options (i.e. headers) that apply to each request. (default: {}, see HttpClient#get|post|...).
+  #
+  #
   def initialize(options = {})
     options = {
       :use_connection_pool => false,
@@ -96,12 +111,46 @@ class HttpClient
     @default_request_options = options[:default_request_options]
   end
 
+  # Execute a GET request
+  #
+  # uri - The full URI of the request. May include a query string, but note that a query string here will be overriden by options.
+  # options - The Hash options used to set headers and the like (optional):
+  #           :headers - A Hash of HTTP headers to send (default: {}).
+  #           :params - A Hash of params that will be turned into a query string using URI::encode_www_form. Overrides any query string in the URI (default: {}).
+  #
+  # Examples
+  #
+  #   client.get("https://www.google.com/robots.txt")
+  #   # => #<HttpClient::Response0xdeadbeef ...>
+  #
+  # Returns an instance of HttpClient::Response.
+  # Raises HttpClient::Timeout if any timeout occurs, retryable.
+  # Raises HttpClient::IOError if any IO error occurs, retryable.
+  # Raises HttpClient::Error if any HTTP protocol error occurs.
   def get(uri, options = {})
     uri = uri.sub(/\?.+$|$/, "?#{URI.encode_www_form(options[:params])}") if options[:params]
     request = create_request(HttpGet, uri, options)
     execute(request)
   end
 
+  # Execute a POST request
+  #
+  # uri - The full URI of the request.
+  # options - The Hash options used to set headers and the like (optional):
+  #           :headers - A Hash of HTTP headers to send, note that a Content-Type header overrides the Content-Type set by :form or :json (default: {}).
+  #           :body - A string that will be sent as the request body (optional).
+  #           :form - A Hash of params that will be turned into a URI encoded body, sets Content-Type header to "application/x-www-form-urlencoded; charset=UTF-8" (optional).
+  #           :json - A Hash of data that will be turned into a JSON body using #to_json, sets Content-Type header to "application/json; charset=UTF-8" (optional).
+  #
+  # Examples
+  #
+  #   client.post("http://www.httpbin.org/post", :form => { :foo => "bar" })
+  #   # => #<HttpClient::Response0xdeadbeef ...>
+  #
+  # Returns an instance of HttpClient::Response.
+  # Raises HttpClient::Timeout if any timeout occurs, retryable.
+  # Raises HttpClient::IOError if any IO error occurs, retryable.
+  # Raises HttpClient::Error if any HTTP protocol error occurs.
   def post(uri, options = {})
     request = create_request(HttpPost, uri, options)
     entity = create_entity(options)
@@ -109,6 +158,24 @@ class HttpClient
     execute(request)
   end
 
+  # Execute a PUT request
+  #
+  # uri - The full URI of the request.
+  # options - The Hash options used to set headers and the like (optional):
+  #           :headers - A Hash of HTTP headers to send, note that a Content-Type header overrides the Content-Type set by :form or :json (default: {}).
+  #           :body - A string that will be sent as the request body (optional).
+  #           :form - A Hash of params that will be turned into a URI encoded body, sets Content-Type header to "application/x-www-form-urlencoded; charset=UTF-8" (optional).
+  #           :json - A Hash of data that will be turned into a JSON body using #to_json, sets Content-Type header to "application/json; charset=UTF-8" (optional).
+  #
+  # Examples
+  #
+  #   client.put("http://www.httpbin.org/put", :json => { :foo => "bar" })
+  #   # => #<HttpClient::Response0xdeadbeef ...>
+  #
+  # Returns an instance of HttpClient::Response.
+  # Raises HttpClient::Timeout if any timeout occurs, retryable.
+  # Raises HttpClient::IOError if any IO error occurs, retryable.
+  # Raises HttpClient::Error if any HTTP protocol error occurs.
   def put(uri, options = {})
     request = create_request(HttpPut, uri, options)
     entity = create_entity(options)
@@ -116,6 +183,24 @@ class HttpClient
     execute(request)
   end
 
+  # Execute a PATCH request
+  #
+  # uri - The full URI of the request.
+  # options - The Hash options used to set headers and the like (optional):
+  #           :headers - A Hash of HTTP headers to send, note that a Content-Type header overrides the Content-Type set by :form or :json (default: {}).
+  #           :body - A string that will be sent as the request body (optional).
+  #           :form - A Hash of params that will be turned into a URI encoded body, sets Content-Type header to "application/x-www-form-urlencoded; charset=UTF-8" (optional).
+  #           :json - A Hash of data that will be turned into a JSON body using #to_json, sets Content-Type header to "application/json; charset=UTF-8" (optional).
+  #
+  # Examples
+  #
+  #   client.patch("http://www.httpbin.org/patch", :body => "foobar")
+  #   # => #<HttpClient::Response0xdeadbeef ...>
+  #
+  # Returns an instance of HttpClient::Response.
+  # Raises HttpClient::Timeout if any timeout occurs, retryable.
+  # Raises HttpClient::IOError if any IO error occurs, retryable.
+  # Raises HttpClient::Error if any HTTP protocol error occurs.
   def patch(uri, options = {})
     request = create_request(HttpPatch, uri, options)
     entity = create_entity(options)
@@ -123,21 +208,75 @@ class HttpClient
     execute(request)
   end
 
+  # Execute a DELETE request
+  #
+  # uri - The full URI of the request.
+  # options - The Hash options used to set headers and the like (optional):
+  #           :headers - A Hash of HTTP headers to send (default: {}).
+  #
+  # Examples
+  #
+  #   client.delete("http://www.httpbin.org/delete", :body => "foobar")
+  #   # => #<HttpClient::Response0xdeadbeef ...>
+  #
+  # Returns an instance of HttpClient::Response.
+  # Raises HttpClient::Timeout if any timeout occurs, retryable.
+  # Raises HttpClient::IOError if any IO error occurs, retryable.
+  # Raises HttpClient::Error if any HTTP protocol error occurs.
   def delete(uri, options = {})
     request = create_request(HttpDelete, uri, options)
     execute(request)
   end
 
+  # Execute a HEAD request
+  #
+  # uri - The full URI of the request.
+  # options - The Hash options used to set headers and the like (optional):
+  #           :headers - A Hash of HTTP headers to send (default: {}).
+  #
+  # Examples
+  #
+  #   client.head("http://www.httpbin.org/delete", :headers => { "X-Auth": "deadbeef42" })
+  #   # => #<HttpClient::Response0xdeadbeef ...>
+  #
+  # Returns an instance of HttpClient::Response.
+  # Raises HttpClient::Timeout if any timeout occurs, retryable.
+  # Raises HttpClient::IOError if any IO error occurs, retryable.
+  # Raises HttpClient::Error if any HTTP protocol error occurs.
   def head(uri, options = {})
     request = create_request(HttpHead, uri, options)
     execute(request)
   end
 
+  # Execute a OPTIONS request
+  #
+  # uri - The full URI of the request.
+  # options - The Hash options used to set headers and the like (optional):
+  #           :headers - A Hash of HTTP headers to send (default: {}).
+  #
+  # Examples
+  #
+  #   client.head("http://www.httpbin.org/delete")
+  #   # => #<HttpClient::Response0xdeadbeef ...>
+  #
+  # Returns an instance of HttpClient::Response.
+  # Raises HttpClient::Timeout if any timeout occurs, retryable.
+  # Raises HttpClient::IOError if any IO error occurs, retryable.
+  # Raises HttpClient::Error if any HTTP protocol error occurs.
   def options(uri, options = {})
     request = create_request(HttpOptions, uri, options)
     execute(request)
   end
 
+  # Reads stats from the connection pool (only available when using a connection pool).
+  #
+  # Examples
+  #
+  #   client.pool_stats
+  #   # => {:idle => 0, :in_use => 2, :max => 3, :waiting => 0}
+  #
+  # Returns a Hash of numbers.
+  # Raises RuntimeError if the client is not configured to use a connection pool.
   def pool_stats
     raise "#{self.class.name}#pool_stats is supported only when using a connection pool" unless @connection_manager.is_a?(PoolingHttpClientConnectionManager)
     total_stats = @connection_manager.total_stats
@@ -149,10 +288,28 @@ class HttpClient
     )
   end
 
+  # Closes idle and expired connections from the pool (only available when using a connection pool).
+  # You may want to call this method on a monitor thread when having long time idling Keep-Alive connections.
+  # This helps against errors when a server closes Keep-Alive connections unexpectedly or earlier than advertised.
+  # And since not all servers send the non-standardized timeout=seconds attribute with a Keep-Alive response,
+  # we may not know how long a connection can be kept open idling.
+  #
+  # max_idle - The maximum time in seconds a connection has been idle.
+  #
+  # Examples
+  #
+  #   client.cleanup_connections(3)
+  #
+  # Returns nothing.
   def cleanup_connections(max_idle = 5)
     @connection_manager.close_idle_connections(max_idle, TimeUnit::SECONDS)
   end
 
+  # Shuts down the connection manager and releases all obtained resources.
+  # This method is thread safe but you should keep in mind that it may cause trouble
+  # for threads that are in the middle of request execution.
+  #
+  # Returns nothing.
   def shutdown
     @connection_manager.shutdown
   end
